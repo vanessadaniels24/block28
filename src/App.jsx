6e40fd104c0b853a9c347b11427083e06d0d53dc
queenNessa <vanessadaniels24@gmail.com>
2024-04-17 14:40:56 -0400

    changes

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import './App.css'
+<React.StrictMode>
+<BrowserRouter>
+  <App />
+</BrowserRouter>
+</React.StrictMode>
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+return 
+}
+
+<div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar"><div id="main-section">
+   <Routes>
+     <Route path="/blue" element={<h1>Blue</h1>} />
+     <Route path="/red" element={<h1>Red</h1>} />
+   </Routes>
+</div></div>
+      <div id="main-section">{}</div>
+    </div>
+      
+
+export default App
